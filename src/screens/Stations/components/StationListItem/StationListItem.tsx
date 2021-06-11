e9ca44799211c3bfe46cafb8e59885a6c6d5d10d
stefan <stefanchelaru96@gmail.com>
2021-06-11 07:55:06 +0300

    +finished stations tab

--- src/screens/Stations/components/StationListItem/StationListItem.tsx
+++ src/screens/Stations/components/StationListItem/StationListItem.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import {Station} from '../../../../types';
-import {ListItem} from '@ui-kitten/components';
+import {ListItem, Text} from '@ui-kitten/components';
+import {useSelector} from 'react-redux';
+import {AppState} from '../../../../store';
+import {strings} from '../../../../locales';
 
 export const StationListItem = ({station: station}: {station: Station}) => {
-  return <ListItem title={station.name} />;
+  const stationWithTrains = useSelector(
+    (state: AppState) => state.timetable.stationWithTrains,
+  );
+
+  return (
+    <ListItem
+      title={station.name}
+      accessoryRight={() => (
+        <Text>{`${
+          stationWithTrains[station.name].length
+        } ${strings.trains.toLowerCase()}`}</Text>
+      )}
+    />
+  );
 };
